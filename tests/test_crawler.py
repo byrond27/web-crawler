dea69c3137517cbeadda92db782f7f7536b71101
Byron Dominguez <byrond27@gmail.com>
2020-04-23 08:56:03 +0200

    fix: test library

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -10,7 +10,8 @@ class MyTestCase(unittest.TestCase):
     topicList = [
         Topic("123", "Lorem ipsum dolor sit amet, consectetur adipiscing elit", "1", "1", "1000"),
         Topic("456", "quis nostrud exercitation ullamco", "2", "500", "200"),
-        Topic("789", "quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo", "3", "0", "55"),
+        Topic("789", "quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo", "3", "0",
+              "55"),
         Topic("321", "Nemo enim ipsam voluptatem quia", "4", "321", "0"),
         Topic("159", "Nemo enim", "5", "22", "23")
     ]
@@ -25,7 +26,8 @@ class MyTestCase(unittest.TestCase):
         expectList = [
             Topic("123", "Lorem ipsum dolor sit amet, consectetur adipiscing elit", "1", "1", "1000"),
             Topic("456", "quis nostrud exercitation ullamco", "2", "500", "200"),
-            Topic("789", "quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo", "3", "0", "55"),
+            Topic("789", "quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo", "3",
+                  "0", "55"),
             Topic("159", "Nemo enim", "5", "22", "23"),
             Topic("321", "Nemo enim ipsam voluptatem quia", "4", "321", "0")
         ]
@@ -33,28 +35,27 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(list(map(vars, actualList)), list(map(vars, expectList)))
 
     def test_should_sort_list_by_points(self):
-
         expectList = [
             Topic("456", "quis nostrud exercitation ullamco", "2", "500", "200"),
             Topic("321", "Nemo enim ipsam voluptatem quia", "4", "321", "0"),
             Topic("159", "Nemo enim", "5", "22", "23"),
             Topic("123", "Lorem ipsum dolor sit amet, consectetur adipiscing elit", "1", "1", "1000"),
-            Topic("789", "quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo", "3", "0", "55")
+            Topic("789", "quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo", "3",
+                  "0", "55")
         ]
         actualList = self.customSorter.sortedByPoints(self.topicList)
         self.assertEqual(list(map(vars, actualList)), list(map(vars, expectList)))
 
     def test_should_return_titles_with_more_than_5_words(self):
-
         expectList = [
             Topic("123", "Lorem ipsum dolor sit amet, consectetur adipiscing elit", "1", "1", "1000"),
-            Topic("789", "quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo", "3", "0", "55")
+            Topic("789", "quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo", "3",
+                  "0", "55")
         ]
         actualList = self.customFilter.filterMoreThanFiveWords(self.topicList)
         self.assertEqual(list(map(vars, actualList)), list(map(vars, expectList)))
 
     def test_should_return_titles_with_less_or_equal_to_5_words(self):
-
         expectList = [
             Topic("456", "quis nostrud exercitation ullamco", "2", "500", "200"),
             Topic("321", "Nemo enim ipsam voluptatem quia", "4", "321", "0"),
@@ -64,23 +65,22 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(list(map(vars, actualList)), list(map(vars, expectList)))
 
     def test_should_return_titles_with_more_than_5_words_sorted_by_comments(self):
-
         expectList = [
             Topic("123", "Lorem ipsum dolor sit amet, consectetur adipiscing elit", "1", "1", "1000"),
-            Topic("789", "quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo", "3", "0", "55")
+            Topic("789", "quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo", "3",
+                  "0", "55")
         ]
         actualList = self.customSorter.sortedByComments(self.customFilter.filterMoreThanFiveWords(self.topicList))
         self.assertEqual(list(map(vars, actualList)), list(map(vars, expectList)))
 
     def test_should_return_titles_with_less_or_equal_to_5_words_sorted_by_points(self):
-
         expectList = [
             Topic("456", "quis nostrud exercitation ullamco", "2", "500", "200"),
             Topic("321", "Nemo enim ipsam voluptatem quia", "4", "321", "0"),
             Topic("159", "Nemo enim", "5", "22", "23"),
         ]
         actualList = self.customSorter.sortedByPoints(self.customFilter.filterMoreLessAndEqualFiveWords(self.topicList))
-        self.assertEqual(list(map(vars,actualList)), list(map(vars, expectList)))
+        self.assertEqual(list(map(vars, actualList)), list(map(vars, expectList)))
 
 
 if __name__ == '__main__':
